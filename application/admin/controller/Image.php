86d1481b226c79349ed27dd5f27d39b6c6424285
ruoxigeng <gengruoxi2013@gmail.com>
2019-05-28 23:48:41 +0800

    upload image

---
+++ application/admin/controller/Image.php
@@ -0,0 +1,20 @@
+<?php
+namespace app\admin\controller;
+
+use think\Controller;
+
+/**
+ * 后台图片上传相关逻辑
+ * Class Image
+ * @package app\admin\controller
+ */
+class Image extends Base
+{
+    /**
+     * 图片上传
+     * @return mixed
+     */
+    public function upload() {
+        return 1;
+    }
+}
