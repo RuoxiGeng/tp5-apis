1a443ea3bb3908d30b2b0d408ac02aeaa7441a2c
ruoxigeng <gengruoxi2013@gmail.com>
2019-05-29 23:51:22 +0800

    upload image

--- application/admin/controller/Image.php
+++ application/admin/controller/Image.php
@@ -2,6 +2,7 @@
 namespace app\admin\controller;
 
 use think\Controller;
+use think\Request;
 
 /**
  * 后台图片上传相关逻辑
@@ -15,6 +16,18 @@ class Image extends Base
      * @return mixed
      */
     public function upload() {
-        return 1;
+        $file = Request::instance()->file('file');
+        $info = $file->move('upload');
+
+        if($info && $info->getPathname()) {
+            $data = [
+                'status' => 1,
+                'message' => 'OK',
+                'data' => '/'.$info->getPathname(),
+            ];
+            echo json_encode($data); exit;
+        }
+
+        echo json_encode([ 'status' => 1, 'message' => 'Fail']);
     }
 }
